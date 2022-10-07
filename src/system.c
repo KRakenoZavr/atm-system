e761535561eb49807e9f0a30e75b0f76a2c81fe2
Arman Kuzembay <kuzembay@gmail.com>
2022-10-07 14:20:24 +0600

    feat: lint, ignore

--- src/system.c
+++ src/system.c
@@ -21,9 +21,9 @@ int getAccountFromFile(FILE *ptr, char name[50], struct Record *r)
 void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
 {
     fprintf(ptr, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n\n",
-            &r->id,
-            &u->id
-            & u->name,
+            r.id,
+            u.id,
+            u.name,
             r.accountNbr,
             r.deposit.month,
             r.deposit.day,
@@ -37,10 +37,11 @@ void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
 void stayOrReturn(int notGood, void f(struct User u), struct User u)
 {
     int option;
-    if (notGood == 0) {
+    if (notGood == 0)
+    {
         system("clear");
         printf("\n✖ Record not found!!\n");
-        invalid:
+            invalid:
         printf("\nEnter 0 to try again, 1 to return to main menu and 2 to exit:");
         scanf("%d", &option);
         if (option == 0)
@@ -49,18 +50,22 @@ void stayOrReturn(int notGood, void f(struct User u), struct User u)
             mainMenu(u);
         else if (option == 2)
             exit(0);
-        else {
+        else
+        {
             printf("Insert a valid operation!\n");
             goto invalid;
         }
-    } else {
+    } else
+    {
         printf("\nEnter 1 to go to the main menu and 0 to exit:");
         scanf("%d", &option);
     }
-    if (option == 1) {
+    if (option == 1)
+    {
         system("clear");
         mainMenu(u);
-    } else {
+    } else
+    {
         system("clear");
         exit(1);
     }
@@ -70,15 +75,18 @@ void success(struct User u)
 {
     int option;
     printf("\n✔ Success!\n\n");
-    invalid:
+        invalid:
     printf("Enter 1 to go to the main menu and 0 to exit!\n");
     scanf("%d", &option);
     system("clear");
-    if (option == 1) {
+    if (option == 1)
+    {
         mainMenu(u);
-    } else if (option == 0) {
+    } else if (option == 0)
+    {
         exit(1);
-    } else {
+    } else
+    {
         printf("Insert a valid operation!\n");
         goto invalid;
     }
@@ -91,7 +99,7 @@ void createNewAcc(struct User u)
     char userName[50];
     FILE *pf = fopen(RECORDS, "a+");
 
-    noAccount:
+        noAccount:
     system("clear");
     printf("\t\t\t===== New record =====\n");
 
@@ -100,8 +108,10 @@ void createNewAcc(struct User u)
     printf("\nEnter the account number:");
     scanf("%d", &r.accountNbr);
 
-    while (getAccountFromFile(pf, userName, &cr)) {
-        if (strcmp(userName, u.name) == 0 && cr.accountNbr == r.accountNbr) {
+    while (getAccountFromFile(pf, userName, &cr))
+    {
+        if (strcmp(userName, u.name) == 0 && cr.accountNbr == r.accountNbr)
+        {
             printf("✖ This Account already exists for this user\n\n");
             goto noAccount;
         }
@@ -130,8 +140,10 @@ void checkAllAccounts(struct User u)
 
     system("clear");
     printf("\t\t====== All accounts from user, %s =====\n\n", u.name);
-    while (getAccountFromFile(pf, userName, &r)) {
-        if (strcmp(userName, u.name) == 0) {
+    while (getAccountFromFile(pf, userName, &r))
+    {
+        if (strcmp(userName, u.name) == 0)
+        {
             printf("_____________________\n");
             printf("\nAccount number:%d\nDeposit Date:%d/%d/%d \ncountry:%s \nPhone number:%d \nAmount deposited: $%.2f \nType Of Account:%s\n",
                    r.accountNbr,
